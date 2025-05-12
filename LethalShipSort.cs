03d90a343be52033bf159c8dd9f05888b811280f
baer1 <videocreator@outlook.de>
2025-05-12 13:20:04 +0200

    Initial Commit

---
+++ LethalShipSort.cs
@@ -0,0 +1,216 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using ChatCommandAPI;
+using Unity.Netcode;
+using UnityEngine;
+using Logger = UnityEngine.Logger;
+using Object = UnityEngine.Object;
+
+namespace LethalShipSort;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+[BepInDependency("baer1.ChatCommandAPI", BepInDependency.DependencyFlags.HardDependency)]
+public class LethalShipSort : BaseUnityPlugin
+{
+    public static LethalShipSort Instance { get; private set; } = null!;
+    internal static new ManualLogSource Logger { get; private set; } = null!;
+
+    private void Awake()
+    {
+        Logger = base.Logger;
+        Instance = this;
+
+        _ = new PositionExtract();
+
+        if (Positions.SCRAP_POSITIONS.Length == 0)
+            Logger.LogError("No scrap positions");
+        if (Positions.TOOL_POSITIONS.Length == 0)
+            Logger.LogError("No tool positions");
+        if (Positions.SCRAP_POSITIONS.Length == 0 || Positions.TOOL_POSITIONS.Length == 0)
+        {
+            Logger.LogError(
+                $"{MyPluginInfo.PLUGIN_GUID} v{MyPluginInfo.PLUGIN_VERSION} has failed to load! (PositionExtract loaded)"
+            );
+            return;
+        }
+
+        _ = new SortItemsCommand();
+
+        Logger.LogInfo($"{MyPluginInfo.PLUGIN_GUID} v{MyPluginInfo.PLUGIN_VERSION} has loaded!");
+    }
+}
+
+public class SortItemsCommand : Command
+{
+    public override string Name => "SortItems";
+    public override string[] Commands => [Name, "Sort", "Organize"];
+    public override string Description => "Sorts all items on the ship";
+
+    public override bool Invoke(string[] args, Dictionary<string, string> kwargs, out string error)
+    {
+        error = "The ship must be in orbit";
+        displayedWarning = false;
+        return StartOfRound.Instance.inShipPhase && SortAllItems(out error);
+    }
+
+    public static readonly Vector3[] ScrapPositions = Positions
+        .SCRAP_POSITIONS.Select(i => new Vector3(i.Item1, Positions.Y, i.Item2))
+        .ToArray();
+    public static readonly Vector3[] ToolPositions = Positions
+        .TOOL_POSITIONS.Select(i => new Vector3(i.Item1, Positions.Y, i.Item2))
+        .ToArray();
+
+    private const string WARNING =
+        "You have too many different items on the ship.\nSome items may be stacked on others";
+    private bool displayedWarning;
+
+    public bool SortAllItems(out string error)
+    {
+        error = "No items to sort";
+        var items = Object.FindObjectsOfType<GrabbableObject>();
+        if (items == null)
+            return false;
+        items = items
+            .Where(i => i is { playerHeldBy: null } and not RagdollGrabbableObject)
+            .ToArray();
+        if (items.Length == 0)
+            return false;
+
+        var scrap = items.Where(i => i.itemProperties.isScrap).ToArray();
+        var tools = items.Where(i => !i.itemProperties.isScrap).ToArray();
+
+        int scrapFailed = 0;
+        if (scrap.Length != 0)
+            scrapFailed = SortItems(scrap, ScrapPositions);
+        int toolsFailed = 0;
+        // ReSharper disable once InvertIf
+        if (tools.Length != 0)
+            toolsFailed = SortItems(tools, ToolPositions);
+
+        error =
+            $"{(scrapFailed > 0 ? $"{scrapFailed} scrap items {(toolsFailed > 0 ? "and " : "")}" : "")}{(toolsFailed > 0 ? $"{toolsFailed} tool items" : "")} couldn't be sorted";
+
+        return scrapFailed == 0 && toolsFailed == 0;
+    }
+
+    private int SortItems(GrabbableObject[] items, Vector3[] positions)
+    {
+        Array.Sort(items, (l, r) => new CaseInsensitiveComparer().Compare(l.name, r.name));
+        Array.Sort(
+            items,
+            (l, r) =>
+                l.itemProperties.twoHanded switch
+                {
+                    true => r.itemProperties.twoHanded ? 0 : -1,
+                    false => r.itemProperties.twoHanded ? 1 : 0,
+                }
+        );
+        string p = null!;
+        int i = 0;
+        int f = 0;
+        foreach (var item in items)
+        {
+            if (item.name != p)
+            {
+                p = item.name;
+                i++;
+            }
+
+            if (!displayedWarning && i > positions.Length)
+            {
+                displayedWarning = true;
+                ChatCommandAPI.ChatCommandAPI.PrintWarning(WARNING);
+            }
+
+            if (!MoveItem(item, positions[i % positions.Length]))
+                f++;
+        }
+
+        return f;
+    }
+
+    private static bool MoveItem(GrabbableObject item, Vector3 position)
+    {
+        LethalShipSort.Logger.LogDebug($"Moving item {item.name} to position {position}");
+        Transform ship = GameObject.Find("Environment/HangarShip").transform;
+        if (ship != null)
+            if (
+                Physics.Raycast(
+                    ship.TransformPoint(position),
+                    Vector3.down,
+                    out var hitInfo,
+                    80f,
+                    268437760,
+                    QueryTriggerInteraction.Ignore
+                )
+            )
+                position = ship.InverseTransformPoint(
+                    hitInfo.point + item.itemProperties.verticalOffset * Vector3.up
+                );
+            else
+                LethalShipSort.Logger.LogWarning("Raycast unsuccessful");
+        else
+            LethalShipSort.Logger.LogWarning("Couldn't find ship");
+
+        GameNetworkManager.Instance.localPlayerController.SetObjectAsNoLongerHeld(
+            true,
+            true,
+            position,
+            item
+        );
+        GameNetworkManager.Instance.localPlayerController.ThrowObjectServerRpc(
+            item.NetworkObject,
+            true,
+            true,
+            position,
+            -1
+        );
+        return true;
+    }
+}
+
+public class PositionExtract : Command
+{
+    public override string[] Commands => ["sortPositionExtract"];
+    public override bool Hidden => true;
+
+    private Transform ship = null!;
+
+    public override bool Invoke(string[] args, Dictionary<string, string> kwargs, out string error)
+    {
+        error = "Couldn't find ship";
+        ship = GameObject.Find("Environment/HangarShip").transform;
+        if (ship == null)
+            return false;
+        StringBuilder sb = new StringBuilder("\n[\n");
+        int i = 0,
+            j = 0;
+        foreach (var kvp in ItemList.GoodItems)
+        {
+            sb.Append(Extract(kvp.Value));
+            i++;
+        }
+        sb.Append("\n");
+        foreach (var kvp in ItemList.BadItems)
+        {
+            sb.Append(Extract(kvp.Value));
+            j++;
+        }
+        sb.Append("];");
+        LethalShipSort.Logger.LogInfo(sb.ToString());
+        ChatCommandAPI.ChatCommandAPI.Print($"Printed {i}+{j} positions to log");
+        return true;
+    }
+
+    private string Extract(Vector3 pos)
+    {
+        Vector3 localPos = ship.InverseTransformPoint(pos);
+        return $"  ({Math.Round(localPos.x, 2)}f, {Math.Round(localPos.z, 2)}f),\n";
+    }
+}
---
+++ Positions.cs
@@ -0,0 +1,100 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LethalShipSort;
+
+public static class Positions
+{
+    public const float Y = 2f;
+
+    public static readonly (float, float)[] SCRAP_POSITIONS =
+    [
+        (-1.96f, -5.26f),
+        (1.75f, -5.96f),
+        (-6.32f, -7.1f),
+        (-2.45f, -6.87f),
+        (-3.14f, -5.85f),
+        (1.62f, -6.73f),
+        (-3.92f, -7f),
+        (-3.33f, -6.91f),
+        (-4.08f, -6.46f),
+        (-5.03f, -7.11f),
+        (-3.48f, -4.93f),
+        (-5.9f, -4.88f),
+        (-6.34f, -7.7f),
+        (-1.25f, -8.46f),
+        (1.29f, -8.36f),
+        (-0.06f, -5.52f),
+        (-1.88f, -6.16f),
+        (8.38f, -6.48f),
+        (-3.93f, -7.45f),
+        (-4.95f, -7.78f),
+        (-3.57f, -8.46f),
+        (6.32f, -5.13f),
+        (-4.98f, -6.25f),
+        (-4.48f, -5.54f),
+        (-0.25f, -8.45f),
+        (-1.8f, -7.05f),
+        (-6.34f, -6.85f),
+        (-6.2f, -6.11f),
+        (2.86f, -6.08f),
+        (8.46f, -7.7f),
+    ];
+    public static readonly (float, float)[] TOOL_POSITIONS = [(0, 0)];
+}
+
+internal static class ItemList
+{
+    internal static readonly Dictionary<string, Vector3> GoodItems = new Dictionary<string, Vector3>
+    {
+        { "Hairdryer", new Vector3(-0.69f, 0.35f, -12.76f) },
+        { "Hairbrush", new Vector3(3.02f, 0.38f, -13.46f) },
+        { "EasterEgg", new Vector3(-5.05f, 0.35f, -14.6f) },
+        { "Mug", new Vector3(-1.18f, 0.35f, -14.37f) },
+        { "Dentures", new Vector3(-1.87f, 0.36f, -13.35f) },
+        { "FancyLamp", new Vector3(2.89f, 0.29f, -14.23f) },
+        { "ComedyMask", new Vector3(-2.65f, 0.35f, -14.5f) },
+        { "FancyRing", new Vector3(-2.06f, 0.35f, -14.41f) },
+        { "TragedyMask", new Vector3(-2.81f, 0.35f, -13.96f) },
+        { "BinFullOfBottles", new Vector3(-3.76f, 0.35f, -14.61f) },
+        { "ToyCube", new Vector3(-2.21f, 0.35f, -12.43f) },
+        { "FancyGlass", new Vector3(-4.63f, 0.35f, -12.38f) },
+        { "FishTestProp", new Vector3(-5.07f, 0.35f, -15.2f) },
+        { "PerfumeBottle", new Vector3(0.02f, 0.35f, -15.96f) },
+        { "Painting", new Vector3(2.56f, 0.29f, -15.86f) },
+        { "Airhorn", new Vector3(1.21f, 0.35f, -13.02f) },
+        { "RedSodaCan", new Vector3(-0.61f, 0.35f, -13.66f) },
+        { "RubberDucky", new Vector3(9.65f, 1.73f, -13.98f) },
+        { "MagnifyingGlass", new Vector3(-2.66f, 0.38f, -14.95f) },
+        { "Toothpaste", new Vector3(-3.68f, 0.35f, -15.28f) },
+        { "Candy", new Vector3(-2.3f, 0.35f, -15.96f) },
+        { "RedLocustHive", new Vector3(7.59f, 0.29f, -12.63f) },
+        { "TeaKettle", new Vector3(-3.71f, 0.35f, -13.75f) },
+        { "HandBell", new Vector3(-3.21f, 0.35f, -13.04f) },
+        { "RobotToy", new Vector3(1.02f, 0.29f, -15.95f) },
+        { "Clownhorn", new Vector3(1.21f, 0.35f, -13.02f) },
+        { "OldPhone", new Vector3(-0.53f, 0.35f, -14.55f) },
+        { "LaserPointer", new Vector3(-5.07f, 0.35f, -14.35f) },
+        { "Magic7Ball", new Vector3(-4.93f, 0.37f, -13.61f) },
+    };
+
+    internal static readonly Dictionary<string, Vector3> BadItems = new Dictionary<string, Vector3>
+    {
+        { "StopSign", new Vector3(4.13f, 0.37f, -13.58f) },
+        { "YieldSign", new Vector3(4.13f, 0.37f, -13.58f) },
+        { "CookieMoldPan", new Vector3(4.13f, 0.37f, -13.58f) },
+        { "DiyFlashbang", new Vector3(4.13f, 0.37f, -13.58f) },
+        { "PillBottle", new Vector3(4.13f, 0.37f, -13.58f) },
+        { "Dustpan", new Vector3(4.13f, 0.37f, -13.58f) },
+        { "SteeringWheel", new Vector3(4.13f, 0.37f, -13.58f) },
+        { "Remote", new Vector3(4.13f, 0.37f, -13.58f) },
+        { "ChemicalJug", new Vector3(4.13f, 0.37f, -13.58f) },
+        { "Flask", new Vector3(4.13f, 0.37f, -13.58f) },
+        { "EnginePart", new Vector3(4.13f, 0.37f, -13.58f) },
+        { "EggBeater", new Vector3(4.13f, 0.37f, -13.58f) },
+        { "BigBolt", new Vector3(4.13f, 0.37f, -13.58f) },
+        { "MetalSheet", new Vector3(4.13f, 0.37f, -13.58f) },
+        { "WhoopieCushion", new Vector3(9.73f, 1.74f, -15.2f) },
+        { "Cog", new Vector3(4.13f, 0.37f, -13.58f) },
+    };
+}
