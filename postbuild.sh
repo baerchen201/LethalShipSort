1fadf41873982568e26aee1fb515618598ed0675
baer1 <videocreator@outlook.de>
2026-09-11 17:39:44 +0200

    Add automated debug build

--- postbuild.sh
+++ postbuild.sh
@@ -1,5 +1,24 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-zip "release.zip" -rMM "BepInEx"
-zip "release.zip" -jMM "$HOME/.nuget/packages/luacsharp/0.5.6/lib/netstandard2.1/Lua.dll" "$HOME/.nuget/packages/luacsharp.annotations/0.5.6/lib/netstandard2.1/Lua.Annotations.dll" "$HOME/.nuget/packages/microsoft.bcl.timeprovider/8.0.0/lib/netstandard2.0/Microsoft.Bcl.TimeProvider.dll" "$HOME/.nuget/packages/system.runtime.compilerservices.unsafe/6.0.0/lib/netstandard2.0/System.Runtime.CompilerServices.Unsafe.dll"
+addzip() {
+  zip "$1" -rMM "BepInEx"
+  zip "$1" -jMM "$HOME/.nuget/packages/luacsharp/0.5.6/lib/netstandard2.1/Lua.dll" "$HOME/.nuget/packages/luacsharp.annotations/0.5.6/lib/netstandard2.1/Lua.Annotations.dll" "$HOME/.nuget/packages/microsoft.bcl.timeprovider/8.0.0/lib/netstandard2.0/Microsoft.Bcl.TimeProvider.dll" "$HOME/.nuget/packages/system.runtime.compilerservices.unsafe/6.0.0/lib/netstandard2.0/System.Runtime.CompilerServices.Unsafe.dll"
+}
+
+echo "Adding dll files to release zip..."
+addzip release.zip
+
+echo -e "\e[1;94m****DEBUG BUILD****\e[0m"
+dotnet build -c Debug -o build-debug
+echo -e "\e[1;94m**** DEBUG ZIP ****\e[0m"
+zip "release-debug.zip" -jMM "manifest.json" build-debug/**.dll
+set +e
+zip "release-debug.zip" -j "icon.png" "README.md"
+_e="$?"
+if [ "$_e" != 0 ] && [ "$_e" != 12 ]; then exit "$_e"; fi
+unset _e
+set -e
+echo "Adding dll files to debug zip..."
+addzip release-debug.zip
+mv -v release-debug.zip release
